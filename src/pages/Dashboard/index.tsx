f0789d1fd3df55dcceb30482f88f2d68e8077e66
Laura <laurabeatriserafim@gmail.com>
2020-05-22 09:31:15 -0300

    feat: add transactions table and button to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,23 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Balance from '../../components/Balance';
+import { FiUpload } from 'react-icons/fi';
+import { formatRelative } from 'date-fns';
 
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
 import total from '../../assets/total.svg';
 
-import api from '../../services/api';
-
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+import Button from '../../styles/components/buttons';
+import TransactionsTable from '../../components/TransactionsTable';
 
 import formatValue from '../../utils/formatValue';
+import api from '../../services/api';
 
-import {
-  Container,
-  CardContainer,
-  Card,
-  TableContainer,
-  Title,
-} from './styles';
+import { Container, CardContainer, Card, Title } from './styles';
 
 interface Transaction {
   id: string;
@@ -48,6 +45,18 @@ const Dashboard: React.FC = () => {
     loadTransactions();
   }, []);
 
+  const transactions = [
+    {
+      id: '1',
+      title: 'Spotify',
+      type: 'Entertainment',
+      income: 900,
+      outcome: 234,
+      category: 'Entertainment',
+      created_at: formatRelative(new Date(), new Date()),
+    },
+  ];
+
   return (
     <>
       <Header />
@@ -60,57 +69,28 @@ const Dashboard: React.FC = () => {
           </span>
         </Title>
         <Balance balance={{ total: 32432 }} />
+        <Button theme="dark">
+          <FiUpload color="#fff" />
+          Import
+        </Button>
         <CardContainer>
           <Card>
             <header>
-              <p>Entradas</p>
+              <p>Incomes</p>
               <img src={income} alt="Income" />
             </header>
             <h1 data-testid="balance-income">R$ 5.000,00</h1>
           </Card>
           <Card>
             <header>
-              <p>Saídas</p>
+              <p>Outcomes</p>
               <img src={outcome} alt="Outcome" />
             </header>
             <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
           </Card>
-          <Card total>
-            <header>
-              <p>Total</p>
-              <img src={total} alt="Total" />
-            </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
-          </Card>
         </CardContainer>
 
-        <TableContainer>
-          <table>
-            <thead>
-              <tr>
-                <th>Título</th>
-                <th>Preço</th>
-                <th>Categoria</th>
-                <th>Data</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
-            </tbody>
-          </table>
-        </TableContainer>
+        <TransactionsTable transactions={transactions} />
       </Container>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -24,8 +24,14 @@ export const Title = styled.h1`
 
 export const CardContainer = styled.section`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   grid-gap: 32px;
+  margin-top: 20px;
+
+  ${media.lessThan('medium')`
+    grid-template-columns: unset;
+    grid-template-rows: auto;
+  `}
 `;
 
 export const Card = styled.div`
@@ -33,6 +39,19 @@ export const Card = styled.div`
   padding: 22px 32px;
   border-radius: 5px;
   color: ${({ total }: CardProps): string => (total ? '#fff' : '#363F5F')};
+  transition: all 400ms;
+  cursor: pointer;
+  box-shadow: 2px -2px 36px -4px rgba(0, 0, 0, 0.31);
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 2px 8px 36px -4px rgba(0, 0, 0, 0.31);
+  }
+
+  &:active {
+    transform: translateY(0px);
+    box-shadow: 2px -2px 36px -4px rgba(0, 0, 0, 0.31);
+  }
 
   header {
     display: flex;
@@ -49,52 +68,5 @@ export const Card = styled.div`
     font-size: 36px;
     font-weight: normal;
     line-height: 54px;
-  }
-`;
-
-export const TableContainer = styled.section`
-  margin-top: 64px;
-
-  table {
-    width: 100%;
-    border-spacing: 0 8px;
-
-    th {
-      color: #969cb3;
-      font-weight: normal;
-      padding: 20px 32px;
-      text-align: left;
-      font-size: 16px;
-      line-height: 24px;
-    }
-
-    td {
-      padding: 20px 32px;
-      border: 0;
-      background: #fff;
-      font-size: 16px;
-      font-weight: normal;
-      color: #969cb3;
-
-      &.title {
-        color: #363f5f;
-      }
-
-      &.income {
-        color: #12a454;
-      }
-
-      &.outcome {
-        color: #e83f5b;
-      }
-    }
-
-    td:first-child {
-      border-radius: 8px 0 0 8px;
-    }
-
-    td:last-child {
-      border-radius: 0 8px 8px 0;
-    }
   }
 `;

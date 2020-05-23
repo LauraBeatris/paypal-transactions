274bd8acc0bcc3b96b5f7071e06e4246349768f2
Laura <laurabeatriserafim@gmail.com>
2020-05-22 23:21:24 -0300

    feat: add transactions types

---
+++ src/types/transactions.ts
@@ -0,0 +1,29 @@
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export type TransactionType = 'income' | 'outcome';
+
+export interface Transaction {
+  id: string;
+  title: string;
+  type: TransactionType;
+  value: number;
+  category: Category;
+  created_at: string;
+}
+
+export interface TransactionsTableProps {
+  transactions: Transaction[];
+}
+
+export interface TransactionRowProps {
+  transaction: Omit<Transaction, 'id'>;
+}
+
+export interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}

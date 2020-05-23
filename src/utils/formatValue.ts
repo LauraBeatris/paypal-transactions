efed255021302c8b4d544753708051d26d0e077b
Laura <laurabeatriserafim@gmail.com>
2020-05-22 23:22:18 -0300

    refactor: update utility function to format currency

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,6 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+    value,
+  );
 
 export default formatValue;

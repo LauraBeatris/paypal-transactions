91d83d5e1811dc9d03fab3475d4ab2c4ae1ca1f6
Laura <laurabeatriserafim@gmail.com>
2020-05-22 23:25:40 -0300

    feat: refactor total balance component

--- src/components/TotalBalance/index.tsx
+++ src/components/TotalBalance/index.tsx
@@ -3,26 +3,28 @@ import { VictoryChart, VictoryAxis, VictoryBar } from 'victory';
 
 import { Container } from './styles';
 
-const data = [
+const mockData = [
   { quarter: 1, balance: 13000 },
   { quarter: 2, balance: 16500 },
   { quarter: 3, balance: 14250 },
 ];
 
 interface BalanceProps {
-  balance: {
-    total: number;
-  };
+  totalBalance: string;
 }
 
-const Balance: React.FC<BalanceProps> = ({ balance }) => {
+const TotalBalance: React.FC<BalanceProps> = ({ totalBalance }) => {
+  // const [balanceSymbol, balanceValue] = totalBalance.split(' ');
+
   return (
     <Container>
       <div>
         <h4>PayPal Balance</h4>
         <h3>
-          <span className="symbol">$</span>
-          <span className="money">1,354</span>
+          {/* <span className="symbol">{balanceSymbol}</span> */}
+          <span className="balance-total" data-testid="balance-total">
+            {totalBalance}
+          </span>
         </h3>
       </div>
 
@@ -31,10 +33,10 @@ const Balance: React.FC<BalanceProps> = ({ balance }) => {
           tickValues={[1, 2, 3]}
           tickFormat={['Mon', 'Tue', 'Wed']}
         />
-        <VictoryBar data={data} x="quarter" y="balance" />
+        <VictoryBar data={mockData} x="quarter" y="balance" />
       </VictoryChart>
     </Container>
   );
 };
 
-export default Balance;
+export default TotalBalance;
